3f63193a2ab00a4effedabafcda2a084a0fceb00
Boiichuk <Boiichuk@A11PC8>
2019-12-16 16:40:23 +0200

    create skeleton

---
+++ api/HD.Dto/ResultDto.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HD.Dto
+{
+    public class ResultDto
+    {
+        bool ok { get; set; }
+        object result { get; set; }
+    }
+}
